40c16ac426fc84bb39235528e955b7219c13315f
kilman14 <ifydollar.mi@gmail.com>
2023-09-25 23:29:46 +0100

    second commit

---
+++ car game.py
@@ -0,0 +1,59 @@
+command = ""
+started = False
+while True:
+    command = input(">..").lower()
+    if command == "start":
+        if started:
+            print("Car already started!")
+        else:
+            started = True
+            print("Car Started...")
+    elif command == "stop":
+        if not started:
+            print("Car already stopped!")
+        else:
+            started = False
+            print("Car stopped.")
+    elif command == "help":
+        print("""
+start ...To start the car
+stop ...To stop the car
+quit ...To end the game
+        """)
+    elif command == "quit":
+        break
+    else:
+        print("""
+Sorry i don't understand
+Type help
+        """)
+
+
+first_name = input("First Name: ")
+last_name = input("Last Name: ")
+year_of_birth = input("Birth Year: ")
+
+print(last_name + first_name)
+year_of_birth = input("Birth Year: ")
+
+
+name = input("NAME> ")
+name = name.upper()
+if name == 'BEN':
+    print('Hello Ben')
+elif name == 'ANITA':
+    print('Hello Anita')
+else:
+    print('Name not found!')
+
+ylist = [1,2,3,4,5,6,7,8,9,10]
+for number in mylist:
+    if number % 2 == 0:
+        print(number)
+    else:
+        print(f"{number} is an odd number")
+
+listsum = 0
+for number in mylist:
+    listsum += number
+print(listsum)
