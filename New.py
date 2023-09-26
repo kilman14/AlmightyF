bfccceed0d2bd31ad97480a53887ba4b7d1ad505
kilman14 <ifydollar.mi@gmail.com>
2023-09-26 15:10:15 +0100

    Third commit

---
+++ New.py
@@ -0,0 +1,23 @@
+# examples of tuples
+my_tuples =(1,2,3,4,5)
+my_tuples2 = ("Apples","Corn","Pea")
+print(my_tuples + my_tuples2)
+print(type(my_tuples))
+
+
+# examples of lists
+my_list = ["Apples","Corn","Pea","ball",'boy']
+print(type(my_list))
+
+# examples of dict
+country_names = {"Nigeria": "Ghana", "Cameroon":"Congo"}
+print(country_names)
+print(type(country_names))
+
+# examples of booleans
+x = "Hello"
+y = 15
+z = ""
+a = 0
+print(bool(z))
+print(bool(a))
